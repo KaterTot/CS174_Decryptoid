9fee322a0477370a2f371abb33a7c60e5f628a19
KaterTot <33531243+KaterTot@users.noreply.github.com>
2019-12-14 21:30:55 -0800

    fixed general layout and functionality
    still need to fix des and doubletrans

--- final.php
+++ final.php
@@ -8,126 +8,9 @@
 	}
 	if (isset($_SESSION['username']) &&
 		($_SESSION['check'] == hash('ripemd128', $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']))) {
-	$output = 'Your translated text will go here.';
 	require_once'credentials.php';
-	$conn = new mysqli($hn, $un, $pw, $db);
-	if($conn->connect_error) die ("Cannot connect to the database.");
-	// LOGOUT Button to redirect to first page
-	echo <<<_END
-		<html><head><title>Decryptoid</title></head>
-		<form method='post' action='logout.php' enctype='multipart/form-data'>
-			<button type="submit">LOGOUT</button>
-		</form>
-_END;
-	// Change this craaaap to JS
-	// Form
-	echo <<<_END
-		<body><form method='post' action='final.php' enctype='multipart/form-data'>
-			<!-- Text Input -->
-			<textarea name="cipher" style="width:600px; height:200px;">Enter your text here or submit a text file below.</textarea><br>
-			<!-- Upload only TXT files -->
-			Select TXT File: <input type='file' name='filename' size='10'>
-			<!-- Decrypt / encrypt button and select Cipher from drop down list -->
-			<select name="cipher" value="Select a Cipher">
-				<option value="cipherSel">---Select Cipher---</option>
-				<option value="simpleSub">Simple Substitution</option>
-				<option value="doubleTrans">Double Transposition</option>
-				<option value="rc4">RC4</option>
-				<option value="des">DES</option>
-			</select><br><br>
-			Input your key: <input type="text" name="key">
-			<button type="submit" name="btnEncrypt">Encrypt</button>
-			<button type="submit" name="btnDecrypt">Decrypt</button>
-		</form>
-_END;
-	// Text output
-	echo <<<_END
-		<textarea name="output" style="width:600px; height:200px;" readonly='readonly'>$output</textarea>
-_END;
-	// Text variable
-	$text = '';
-	// Checking if a file was uploaded
-	if($_FILES)
-	{
-		$name = $_FILES['filename']['name'];
-		$name = strtolower(preg_replace("[^A-Za-z0-9]", "", $name));
-		if($_FILES['filename']['type'] == 'text/plain')
-		{
-			echo "Uploaded text file: '$name'<br>";
-			$fh = fopen($name, 'r') or die("<br>Failed to open file<br>");
-			$text = mysql_entities_fix_string($conn, fgets($fh));
-				
-			fclose($fh);
-		}
-		else
-		{
-			echo "<br>You can only upload txt files.<br>";
-		}
-	}
-	else if(isset($_POST['cipher']))
-	{
-		$text = mysql_entities_fix_string($conn, $_POST['cipher']);
-	}
-	else
-	{
-		echo "<br>You must either enter a text or submit a txt file.<br>";
-	}
-	echo "</body></html>";
-	// Checking which button was pressed
-	$action = '';
-	$key = '';
-	$cipher = '';
-	if(isset($_POST['btnEncrypt'])) $action = 'encrypt';
-	else if(isset($_POST['btnDecrypt'])) $action = 'decrypt';
-	// Checking for key
-	if (!isset($_POST['key'])) echo "<br>You must enter a key.<br>";
-	else $key = mysql_entities_fix_string($conn, $_POST['key']);
-	// Checking which cipher was selected
-	if (isset($_POST['cipher'])) $cipher = mysql_entities_fix_string($conn, $_POST['cipher']);
-	
-	if($cipher !== 'cipherSel')
-	{
-		if($cipher === 'simpleSub') $output = simpleSub($text, $key, $action);
-		else if($cipher === 'doubleTrans') $output = doubleTrans($text, $key, $action);
-		else if($cipher === 'rc4') $output = rc4($text, $key);
-		else if($cipher === 'des') $output = des($text, $key, $action);
-	}
-	else echo "<br>You must select a cipher.<br>";
-	// Inserting into database
-	$query = "INSERT INTO cipherbank (uID, input, cipher, output, cKey) VALUES ('$_SESSION['uID']','$text', '$cipher', '$output', '$key')";
-	$result = $conn->query($query);
-	if(!$result) die("Query failed. Cannot add the cipher to the database.<br><br>");
-	// Printing a table of all inputs from user
-	$query = "SELECT * FROM cipherbank WHERE uID = '$_SESSION['uID']'";
-	$result = $conn->query($query);
-	if(!$result) die ("Query failed. Cannot connect to database.");
-	$rows = $result->num_rows;
-	echo <<<_END
-		<pre><table style="width:50%">
-		<tr>
-			<th>Input Time</th>
-			<th>Cipher</th>
-			<th>Key</th>
-			<th>Input</th>
-			<th>Output</th>
-		</tr>
-_END;
-	for($i = 0; $i < $rows; $i++)
-	{
-		$result->data_seek($i);
-		$row = $result->fetch_array(MYSQLI_NUM);
-		$result->close();
-		echo <<<_END
-		<tr>
-			<td style="text-align:center">$row[1]</td>
-			<td style="text-align:center">$row[3]</td>
-			<td style="text-align:center">$row[2]</td>
-			<td style="text-align:center">$row[4]</td>
-			<td style="text-align:center">$row[5]</td>
-		</tr>
-_END;
-		echo "</table></pre>";
-	}
+
+	// START PHP FUNCTIONS ---------------------------------------------------------------------------------------
 	// Sanitization functions
 	function mysql_entities_fix_string($connection, $string)
 	{
@@ -138,12 +21,11 @@ _END;
 		if(get_magic_quotes_gpc()) $string = stripslashes($string);
 		return $connection->real_escape_string($string);
 	}
-	$conn->close();
 	function simpleSub($text, $key, $action)
 	{
-		if ($key > 26 || $key < 1)
+		if (strlen($key) > 26 || strlen($key) < 1)
 		{
-			return "key must be 26 letters or less";
+			die("key must be 26 letters or less");
 		}
 		$letters = 'abcdefghijklmnopqrstuvwxyz';
 		$hold = $letters;
@@ -181,9 +63,9 @@ _END;
 	}
 	function transp($text, $key, $action)
 	{
-		if ($key > 26 || $key < 1)
+		if (strlen($key) > 26 || strlen($key) < 1)
 		{
-			return "key must be 26 letters or less";
+			die("key must be 26 letters or less");
 		}
 		$letters = 'abcdefghijklmnopqrstuvwxyz';
 		$text = strtolower($text);
@@ -464,5 +346,131 @@ _END;
 		}
 		return $result;
 	}
+
+	// END PHP FUNCTIONS -----------------------------------------------------------------------------------------
+
+	$conn = new mysqli($hn, $un, $pw, $db);
+	if($conn->connect_error) die ("Cannot connect to the database.");
+	// LOGOUT Button to redirect to first page
+	echo <<<_END
+		<html><head><title>Decryptoid</title></head>
+		<form method='post' action='logout.php' enctype='multipart/form-data'>
+			<button type="submit">LOGOUT</button>
+		</form>
+_END;
+
+	// Form
+	echo <<<_END
+		<body><form method='post' action='final.php' enctype='multipart/form-data'>
+			<!-- Text Input -->
+			<textarea name="input" style="width:600px; height:200px;">Enter your text here or submit a text file below.</textarea><br>
+			<!-- Upload only TXT files -->
+			Select TXT File: <input type='file' name='filename' size='10'>
+			<!-- Decrypt / encrypt button and select Cipher from drop down list -->
+			<select name="cipher" value="Select a Cipher">
+				<option value="cipherSel">---Select Cipher---</option>
+				<option value="simpleSub">Simple Substitution</option>
+				<option value="doubleTrans">Double Transposition</option>
+				<option value="rc4">RC4</option>
+				<option value="des">DES</option>
+			</select><br><br>
+			Input your key: <input type="text" name="key">
+			<button type="submit" name="btnEncrypt">Encrypt</button>
+			<button type="submit" name="btnDecrypt">Decrypt</button>
+		</form>
+_END;
+
+	// Text variable
+	$text = '';
+	$output = '';
+	// Checking if a file was uploaded
+	if(isset($_FILES['file']['name']))
+	{
+		$name = $_FILES['filename']['name'];
+		$name = strtolower(preg_replace("[^A-Za-z0-9]", "", $name));
+		if($_FILES['filename']['type'] == 'text/plain')
+		{
+			echo "Uploaded text file: '$name'<br>";
+			$fh = fopen($name, 'r') or die("<br>Failed to open file<br>");
+			$text = mysql_entities_fix_string($conn, fgets($fh));
+				
+			fclose($fh);
+		}
+		else
+		{
+			die("<br>You can only upload txt files.<br>");
+		}
+	}
+	else if(isset($_POST['input']))
+	{
+		$text = mysql_entities_fix_string($conn, $_POST['input']);
+	}
+	else
+	{
+		die("<br>You must either enter a text or submit a txt file.<br>");
+	}
+	echo "</body></html>";
+	// Checking which button was pressed
+	$action = '';
+	$key = '';
+	$cipher = '';
+	if(isset($_POST['btnEncrypt'])) $action = 'Encrypt';
+	else if(isset($_POST['btnDecrypt'])) $action = 'Decrypt';
+	// Checking for key
+	if (!isset($_POST['key'])) echo "<br>You must enter a key.<br>";
+	else $key = mysql_entities_fix_string($conn, $_POST['key']);
+	// Checking which cipher was selected
+	if (isset($_POST['cipher'])) $cipher = mysql_entities_fix_string($conn, $_POST['cipher']);
+	
+	if($cipher !== 'cipherSel')
+	{
+		if($cipher === 'simpleSub') $output = simpleSub($text, $key, $action);
+		else if($cipher === 'doubleTrans') $output = doubleTrans($text, $key, $action);
+		else if($cipher === 'rc4') $output = rc4($text, $key);
+		else if($cipher === 'des') $output = des($text, $key, $action);
+		echo "Output: ".$output;
+	}
+	else die("<br>You must select a cipher.<br>");
+	// Inserting into database
+	$uName = $_SESSION['username'];
+	$query = "INSERT INTO cipherbank (username, input, cipher, output, cKey, method) VALUES ('$uName','$text', '$cipher', '$output', '$key', '$action')";
+	$result = $conn->query($query);
+	if(!$result) die("Query failed. Cannot add the cipher to the database.<br><br>");
+	// Printing a table of all inputs from user
+	$query = "SELECT * FROM cipherbank WHERE username = '$uName'";
+	$result = $conn->query($query);
+	if(!$result) die ("Query failed. Cannot connect to database.");
+	$rows = $result->num_rows;
+	echo <<<_END
+		<pre><table style="width:50%">
+		<tr>
+			<th>Input Time</th>
+			<th>Cipher</th>
+			<th>Key</th>
+			<th>Input</th>
+			<th>Output</th>
+			<th>Method</th>
+		</tr>
+_END;
+	for($i = 0; $i < $rows; $i++)
+	{
+		$result->data_seek($i);
+		$row = $result->fetch_array(MYSQLI_NUM);
+
+		echo <<<_END
+		<tr>
+			<td style="text-align:center">$row[1]</td>
+			<td style="text-align:center">$row[3]</td>
+			<td style="text-align:center">$row[5]</td>
+			<td style="text-align:center">$row[2]</td>
+			<td style="text-align:center">$row[4]</td>
+			<td style="text-align:center">$row[6]</td>
+		</tr>
+_END;
+	}
+	echo "</table></pre>";
+	$result->close();
+	$conn->close();
+	
 } else echo "Please <a href='logout.php'>click here</a> to log in.";
 ?>
